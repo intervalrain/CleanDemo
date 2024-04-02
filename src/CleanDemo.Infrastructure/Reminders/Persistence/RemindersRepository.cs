d966d74e0f1b161fe8b002aac24caddde0b8775d
Rain Hu <intervalrain@gmail.com>
2024-04-02 23:55:29 +0800

    impl repositories with inMemoryRepositories

---
+++ src/CleanDemo.Infrastructure/Reminders/Persistence/RemindersRepository.cs
@@ -0,0 +1,34 @@
+﻿using CleanDemo.Application.Common.Interfaces;
+using CleanDemo.Domain.Reminders;
+
+namespace CleanDemo.Infrastructure.Reminders.Persistence;
+
+public class RemindersRepository : IRemindersRepository
+{
+    private readonly Dictionary<Guid, Reminder> _reminders = new(); 
+
+    public async Task AddReminderAsync(Reminder reminder)
+    {
+        await Task.Run(() =>
+        {
+            if (_reminders.ContainsKey(reminder.Id))
+            {
+                throw new InvalidOperationException();
+            }
+            _reminders.Add(reminder.Id, reminder);
+        });
+    }
+
+    public async Task<Reminder?> GetReminderByIdAsync(Guid reminderId)
+    {
+        return await Task.Run(() =>
+        {
+            if (!_reminders.ContainsKey(reminderId))
+            {
+                throw new KeyNotFoundException();
+            }
+            return _reminders[reminderId];
+        });
+    }
+}
+
---
+++ src/CleanDemo.Infrastructure/Users/Persistence/UsersRepository.cs
@@ -0,0 +1,41 @@
+﻿using CleanDemo.Application.Common.Interfaces;
+using CleanDemo.Domain.Users;
+
+namespace CleanDemo.Infrastructure.Users.Persistence;
+
+public class UsersRepository : IUsersRepository
+{
+    private readonly Dictionary<Guid, User> _users = new();
+
+    public async Task AddUserAsync(User user)
+    {
+        await Task.Run(() =>
+        {
+            if (_users.ContainsKey(user.Id))
+            {
+                throw new InvalidOperationException();
+            }
+            _users.Add(user.Id, user);
+        });
+    }
+
+    public async Task<User?> GetByIdAsync(Guid userId)
+    {
+        return await Task.Run(() =>
+        {
+            if (!_users.ContainsKey(userId))
+            {
+                throw new KeyNotFoundException();
+            }
+            return _users[userId];
+        });
+    }
+
+    public async Task UpdateUserAsync(User user)
+    {
+        await Task.Run(() =>
+        {
+            _users[user.Id] = user;
+        });
+    }
+}
