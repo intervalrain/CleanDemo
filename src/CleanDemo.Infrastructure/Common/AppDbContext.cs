0181c46e2e3812f5cd355fd97cf6bc4cbd7d7b96
Rain Hu <intervalrain@gmail.com>
2024-04-03 00:28:58 +0800

    Setup infrastructure - AppDbContext, EventualConsistencyMiddleware

---
+++ src/CleanDemo.Infrastructure/Common/AppDbContext.cs
@@ -0,0 +1,74 @@
+﻿using CleanDemo.Domain.Common;
+using CleanDemo.Domain.Reminders;
+using CleanDemo.Domain.Users;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanDemo.Infrastructure.Common;
+
+public class AppDbContext : DbContext
+{
+    private readonly DbContextOptions _options;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IPublisher _publisher;
+
+    public DbSet<Reminder> Reminders { get; set; } = null!;
+
+    public DbSet<User> Users { get; set; } = null!; 
+
+    public AppDbContext(DbContextOptions options, IHttpContextAccessor httpContextAccessor, IPublisher publisher)
+        : base(options)
+    {
+        _options = options;
+        _httpContextAccessor = httpContextAccessor;
+        _publisher = publisher;
+    }
+
+    public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEvents = ChangeTracker.Entries<Entity>()
+            .Select(entry => entry.Entity.PopDomainEvents())
+            .SelectMany(x => x)
+            .ToList();
+
+        if (IsUserWaitingOnline())
+        {
+            AddDomainEventsToOfflineProcessingQueue(domainEvents);
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        await PublishDomainEvents(domainEvents);
+        return await base.SaveChangesAsync(cancellationToken); 
+    }
+
+    private void AddDomainEventsToOfflineProcessingQueue(List<IDomainEvent> domainEvents)
+    {
+        var queue = _httpContextAccessor.HttpContext.Items.TryGetValue(EventualConsistencyMiddleware.DomainEventsKey, out var value) &&
+            value is Queue<IDomainEvent> events
+            ? events
+            : new();
+
+        domainEvents.ForEach(queue.Enqueue);
+        _httpContextAccessor.HttpContext.Items[EventualConsistencyMiddleware.DomainEventsKey] = queue;
+    }
+
+    private async Task PublishDomainEvents(List<IDomainEvent> domainEvents)
+    {
+        foreach (var @event in domainEvents)
+        {
+            await _publisher.Publish(@event);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    private bool IsUserWaitingOnline() => _httpContextAccessor.HttpContext is not null;
+}
+
---
+++ src/CleanDemo.Infrastructure/Common/EventualConsistencyMiddleware.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanDemo.Infrastructure.Common;
+
+public class EventualConsistencyMiddleware
+{
+    public const string DomainEventsKey = "DomainEventsKey";
+}
+
