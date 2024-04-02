00ed00a07b2d0772e0e13210596ef845de3b22d6
Rain Hu <intervalrain@gmail.com>
2024-04-03 02:29:40 +0800

    Impl EventualConsistencyMiddleware

--- src/CleanDemo.Infrastructure/Common/EventualConsistencyMiddleware.cs
+++ src/CleanDemo.Infrastructure/Common/EventualConsistencyMiddleware.cs
@@ -1,7 +1,50 @@
-﻿namespace CleanDemo.Infrastructure.Common;
+﻿using CleanDemo.Domain.Common;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Http;
+
+namespace CleanDemo.Infrastructure.Common;
 
 public class EventualConsistencyMiddleware
 {
     public const string DomainEventsKey = "DomainEventsKey";
+
+    private readonly RequestDelegate _next;
+
+    public EventualConsistencyMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, IPublisher publisher, AppDbContext dbContext)
+    {
+        var transaction = dbContext.Database.BeginTransaction();
+        context.Response.OnCompleted(async () =>
+        {
+            try
+            {
+                if (context.Items.TryGetValue(DomainEventsKey, out var value) && value is Queue<IDomainEvent> events)
+                {
+                    while (events.TryDequeue(out var @event))
+                    {
+                        await publisher.Publish(@event);
+                    }
+                }
+
+                await transaction.CommitAsync();
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+            }
+            finally
+            {
+                await transaction.DisposeAsync();
+            }
+        });
+
+        await _next(context);
+    }
 }
 
