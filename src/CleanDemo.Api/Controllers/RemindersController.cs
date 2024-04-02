3d12edcabf32e836aa6ab0ebcc4b5a0f88c77452
Rain Hu <intervalrain@gmail.com>
2024-04-02 18:27:04 +0800

    add controller for reminders

---
+++ src/CleanDemo.Api/Controllers/RemindersController.cs
@@ -0,0 +1,34 @@
+﻿using CleanDemo.Contracts.Reminders;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanDemo.Api.Controllers;
+
+[Route("[Controller]")]
+public class RemindersController : ApiController
+{
+    private readonly ISender _mediator;
+
+    public RemindersController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateReminder(CreateReminderRequest request)
+    {
+        var response = new ReminderResponse(request.Text, request.DateTime);
+        return Ok(response);
+    }
+
+    [HttpGet("{reminderId:guid}")]
+    public async Task<IActionResult> GetReminder(Guid reminderId)
+    {
+        var response = new ReminderResponse("text", DateTimeOffset.UtcNow);
+        return Ok(response);
+
+    }
+}
+
