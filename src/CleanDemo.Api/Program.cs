745c6db646bd34fe44a1b84bbe6578f5f1101348
Rain Hu <intervalrain@gmail.com>
2024-04-03 02:31:47 +0800

    inject EventualConsistencyMiddleware

--- src/CleanDemo.Api/Program.cs
+++ src/CleanDemo.Api/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
+    app.UseInfrastructure();
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
---
+++ src/CleanDemo.Infrastructure/RequestPipeline.cs
@@ -0,0 +1,15 @@
+﻿using CleanDemo.Infrastructure.Common;
+
+using Microsoft.AspNetCore.Builder;
+
+namespace CleanDemo.Infrastructure;
+
+public static class RequestPipeline
+{
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<EventualConsistencyMiddleware>();
+        return app;
+    }
+}
+
