2aaddeed314a78efaec0c5813455b3215ac525f9
Rain Hu <intervalrain@gmail.com>
2024-04-02 18:17:05 +0800

    add ApiController

---
+++ src/CleanDemo.Api/Controllers/ApiController.cs
@@ -0,0 +1,49 @@
+﻿using ErrorOr;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CleanDemo.Api.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    protected IActionResult Problem(List<Error> errors)
+    {
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(e => e.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Unauthorized => StatusCodes.Status403Forbidden,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        errors.ForEach(e => modelStateDictionary.AddModelError(e.Code, e.Description);
+
+        return ValidationProblem(modelStateDictionary);
+    }
+}
+
